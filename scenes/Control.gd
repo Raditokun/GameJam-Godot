ff0d7c68234d42e89d43bd5901942f1d739a642b
Raditokun <radit040407@gmail.com>
2026-08-02 23:43:20 +0700

    kepin knntl

--- scenes/Control.gd
+++ scenes/Control.gd
@@ -1,27 +1,28 @@
 extends Control
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_start_pressed() -> void:
+	# This loads the game. 
+	# Make sure the "Start" button's signal is connected to THIS function!
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
 
-
 func _on_setting_pressed() -> void:
+	# This loads the settings. 
+	# Make sure the "Setting" button's signal is connected to THIS function!
 	get_tree().change_scene_to_file("res://scenes/SettingsMenu.tscn")
 
-
 func _on_credits_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/sword.tscn")
-
+	# FIXED CRASH: "sword.tscn" was deleted, so this would have broken the game.
+	# It will now just print a message to the console until you make a real Credits scene.
+	print("Credits button clicked! (Need to make a Credits.tscn)")
+	# get_tree().change_scene_to_file("res://scenes/Credits.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
